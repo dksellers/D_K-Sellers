2597d6072ee7026213bced5116266cb3933db52b
dksellers <116655317+dksellers@users.noreply.github.com>
2022-10-25 20:10:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,26 +36,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
+global.vcardowner = ['94715530765'] //ur owner number
+global.ownername = "Chamod Kavinda" //ur owner name
+global.ytname = "YT:D_K T World" //ur yt chanel name
+global.socialm = "FACEBOOK:D_K Sellers" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.owner = ['94715530765']
+global.ownertag = '94715530765' //ur tag number
+global.botnumber = '94715530765' //Add your Bot Number For Log
+global.botname = 'D_K S SUPPORT BOT' //ur bot name
+global.ig = "https://youtube.com/channel/UCbNDmPC8UZtU28E_yj2Tq3w" //your theme url which will be displayed on whatsapp
+global.myweb = "https://youtube.com/channel/UCbNDmPC8UZtU28E_yj2Tq3w" //ur website to be displayed
+global.sc = 'https://www.facebook.com/Dkonlinesallercenterchamodkavinda' //script link
+global.linkz = 'https://youtube.com/channel/UCbNDmPC8UZtU28E_yj2Tq3w'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.packname = "D_K S SUPPORT BOT" //ur sticker watermark packname
+global.author = "D_K Sellers" //ur sticker watermark author
+global.footer = "D_K SELLERS"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
